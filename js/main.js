27a5d4618773513b45e2a79f6657fb38a57f74d2
Kelvin Fichter <kelvinfichter@gmail.com>
2018-08-10 19:22:03 -0700

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const populateTable = (data) => {
+  let lines = data.split("\n");
+  let output = [];
+  for (let i = 1; i < lines.length; i++) {
+    let splitLine = lines[i].slice(0,-1).split(",");
+    output.push(`
+      <tr>
+        <td>${splitLine[0]}</td>
+        <td>${splitLine[1]}</td>
+        <td>${splitLine[2]}</td>
+        <td>${splitLine[3]}</td>
+      </tr>
+    `);
+  }
+  document.getElementById("gas-table-body").innerHTML = output.join("");
+};
+
+fetch("data/gasprices.csv")
+  .then(response => response.text())
+  .then(data => populateTable(data));
